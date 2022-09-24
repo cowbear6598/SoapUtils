0acde10a82f849a8c2b8ecdd209b489e2105c20b
CptSoap <cowbear6598@gmail.com>
2022-09-25 00:13:53 +0800

    1. add Plugins 2. add VContainerUnitTest.cs

---
+++ Assets/SoapUtils/Editor/TestFramework/VContainerUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using VContainer;
+
+namespace SoapUtils.Editor.TestFramework
+{
+    public abstract class VContainerUnitTest
+    {
+        protected ContainerBuilder builder;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            builder = new ContainerBuilder();
+        }
+    }
+}
