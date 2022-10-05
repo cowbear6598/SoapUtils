e1f02403cf45e0026bf0c89fedf009a755bdd1a1
CptSoap <cowbear6598@gmail.com>
2022-10-05 23:58:11 +0800

    add bubble sort & change multiple game objects' name

---
+++ Assets/SoapUtils/Editor/Tools/ChangeMultipleGameObjectsName.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using SoapUtils.Runtime.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace SoapUtils.Editor.Tools
+{
+    public class ChangeMultipleGameObjectsName : EditorWindow
+    {
+        [MenuItem("Soap/ChangeMultipleGameObjectsName")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<ChangeMultipleGameObjectsName>();
+            window.titleContent = new GUIContent("Setting");
+            window.Show();
+        }
+
+        private string prefixesName = "";
+        private string suffixesName = "";
+        private int    startOrder   = 0;
+
+        private class GameObjectOriginData
+        {
+            public Transform parent;
+            public Vector3   localPosition;
+
+            public GameObjectOriginData(Transform parent, Vector3 localPosition)
+            {
+                this.parent        = parent;
+                this.localPosition = localPosition;
+            }
+        }
+
+        private Dictionary<int, GameObjectOriginData> originDataDictionary = new(); 
+
+        private void OnGUI()
+        {
+            prefixesName = EditorGUILayout.TextField("Prefixes Name: ", prefixesName);
+            suffixesName = EditorGUILayout.TextField("Suffixes Name: ", suffixesName);
+            startOrder   = EditorGUILayout.IntField("Start Order: ", startOrder);
+
+            if (GUILayout.Button("Change"))
+            {
+                Change();
+            }
+        }
+
+        private void Change()
+        {
+            int          startIndex   = startOrder;
+            
+            Transform[] selectedObjs   = Selection.transforms;
+            int[]       selectedObjsOrder = new int[selectedObjs.Length];
+
+            for (int i = 0; i < selectedObjs.Length; i++)
+            {
+                selectedObjsOrder[i] = selectedObjs[i].transform.root.GetSiblingIndex();
+            }
+
+            Transform[] orderObjs = SortUtils.BubbleSort(selectedObjsOrder, selectedObjs);
+
+            for (int i = 0; i < orderObjs.Length; i++)
+            {
+                orderObjs[i].name = prefixesName + startIndex + suffixesName;
+                startIndex++;
+            }
+        }
+    }
+}
---
+++ Assets/SoapUtils/Runtime/Utils/SortUtils.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SoapUtils.Runtime.Utils
+{
+    public static class SortUtils
+    {
+        public static T[] BubbleSort<T>(int[] orderArray, T[] sortArray)
+        {
+            if (orderArray.Length != sortArray.Length)
+            {
+                Debug.LogWarning("array length not equal");
+                return null;
+            }
+
+            int len = sortArray.Length;
+
+            for (int i = 1; i < len; i++) // 排序次數
+            {
+                for (int j = 1; j < len; j++) // 執行次數
+                {
+                    if (orderArray[j] < orderArray[j - 1])
+                    {
+                        int tempOrder = orderArray[j];
+                        T   temp  = sortArray[j];
+
+                        orderArray[j] = orderArray[j - 1];
+                        sortArray[j]  = sortArray[j  - 1];
+
+                        orderArray[j - 1] = tempOrder;
+                        sortArray[j  - 1] = temp;
+                    }
+                }
+            }
+            
+            return sortArray;
+        }
+    }
+}
