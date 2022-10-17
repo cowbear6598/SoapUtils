1b6c6e5fee0508c0e6294faf5dd1cd2248f59012
cowbear6598 <cowbear6598@gmail.com>
2022-10-17 15:49:22 +0800

    complete database system

---
+++ Assets/SoapUtils/Runtime/DatabaseSystem/DatabaseSystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using Zenject;
+
+namespace SoapUtils.Runtime.DatabaseSystem
+{
+    public class DatabaseSystem
+    {
+        [Inject] private Settings settings;
+
+        public async UniTask<string> Get(int domainIndex, string api, params string[] data)
+        {
+            using UnityWebRequest req = UnityWebRequest.Get(urlParams(domainIndex, api, data));
+            
+            req.timeout = settings.timeout;
+            await req.SendWebRequest();
+            
+            if (req.result == UnityWebRequest.Result.Success)
+            {
+#if UNITY_EDITOR
+                Debug.Log($"{req.url}: \n {req.downloadHandler.text}");
+#endif
+                return req.downloadHandler.text;
+            }
+
+            throw new Exception(req.error);
+        }
+        
+        public async UniTask<string> Post(int domainIndex, string api, object data)
+        {
+            using UnityWebRequest req = new UnityWebRequest(url(domainIndex, api), "POST");
+            req.uploadHandler   = GetUploadHandler(data);
+            req.downloadHandler = new DownloadHandlerBuffer();
+            req.SetRequestHeader("Content-Type", "application/json");
+            req.timeout = settings.timeout;
+            
+            await req.SendWebRequest();
+            
+            if (req.result == UnityWebRequest.Result.Success)
+            {
+#if UNITY_EDITOR
+                Debug.Log($"{req.url}: \n {req.downloadHandler.text}");
+#endif
+                return req.downloadHandler.text;
+            }
+
+            throw new Exception(req.error);
+        }
+        
+        private string url(int domainIndex, string api) => $"{settings.domains[domainIndex]}{api}";
+        private string urlParams(int domainIndex, string api, params string[] data)
+        {
+            string finalUrl = url(domainIndex, api) + "?";
+
+            for (int i = 0; i < data.Length; i++)
+                finalUrl += data[i];
+
+            return finalUrl;
+        }
+
+        private UploadHandler GetUploadHandler(object data)
+        {
+            if (data == null) return null;
+
+            byte[] jsonRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
+
+            UploadHandler uploadHandler = new UploadHandlerRaw(jsonRaw);
+            uploadHandler.contentType = "application/json";
+
+            return uploadHandler;
+        }
+        
+        [Serializable]
+        public class Settings
+        {
+            public string[] domains;
+            public int      timeout;
+        }
+    }
+}
---
+++ Assets/SoapUtils/Runtime/DatabaseSystem/DatabaseSystemInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace SoapUtils.Runtime.DatabaseSystem
+{
+    [CreateAssetMenu(menuName = "Soap/DatabaseSettings")]
+    public class DatabaseSystemInstaller : ScriptableObjectInstaller<DatabaseSystemInstaller>
+    {
+        public DatabaseSystem.Settings settings;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(settings).IfNotBound();
+        }
+    }
+}
