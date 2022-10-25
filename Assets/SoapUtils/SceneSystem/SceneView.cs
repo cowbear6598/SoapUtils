ef65daa2d7b5432c1b6f197f8821e4622ff852ba
CptSoap <cowbear6598@gmail.com>
2022-10-25 22:57:37 +0800

    add loading rotate

--- Assets/SoapUtils/SceneSystem/SceneView.cs
+++ Assets/SoapUtils/SceneSystem/SceneView.cs
@@ -1,4 +1,5 @@
 ﻿using AnimeTask;
+using UniRx;
 using UnityEngine;
 
 namespace SoapUtils.SceneSystem
@@ -7,16 +8,35 @@ namespace SoapUtils.SceneSystem
     {
         private CanvasGroup canvasGroup;
 
+        [SerializeField] private RectTransform loadingTrans;
+        [SerializeField] private float         rotateSpeed;
+        
+        private CompositeDisposable updateDisposable;
+            
         private void Awake()
         {
             canvasGroup = GetComponent<CanvasGroup>();
         }
 
+        private void UpdateEvent(long frame)
+        {
+            loadingTrans.Rotate(new Vector3(0, 0, rotateSpeed * Time.deltaTime), Space.Self);
+        }
+        
         public async void SetAppear(bool IsOn)
         {
+            if (IsOn)
+            {
+                updateDisposable = new CompositeDisposable();
+                Observable.EveryUpdate().Subscribe(UpdateEvent).AddTo(updateDisposable);
+            }
+
             canvasGroup.interactable = canvasGroup.blocksRaycasts = IsOn;
 
             await Easing.Create<Linear>(IsOn ? 1 : 0, 0.25f).ToColorA(canvasGroup);
+            
+            if(!IsOn)
+                updateDisposable.Dispose();
         }
     }
 }
