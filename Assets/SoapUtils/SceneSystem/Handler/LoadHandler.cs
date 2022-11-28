eb083557bb6f22537bfa75760ba78948a6dd5e12
cowbear6598 <cowbear6598@gmail.com>
2022-11-28 10:16:43 +0800

    change scene load to addressable and fix clip didn't release bug

--- Assets/SoapUtils/SceneSystem/Handler/LoadHandler.cs
+++ Assets/SoapUtils/SceneSystem/Handler/LoadHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
-using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
